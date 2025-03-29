4f8b8695dd4605aad75e5e257dcb1d6d4f0b7b10
Marco Chavez <marcode.chavez.jr@gmail.com>
2025-03-29 13:12:51 -0700

    feat(navbar): creates  a navbar to store links

---
+++ components/navbar.vue
@@ -0,0 +1,22 @@
+<template>
+	<nav class="p-6 flex justify-between border-b border-white">
+		<h1
+			:class="[underlineOnHover, 'cursor-pointer']"
+			role="link"
+			@click="$router.push('/')"
+			@keydown.enter.space="$router.push('/')"
+		>Marco Chavez</h1>
+
+		<ul>
+			<li>
+				<NuxtLink :class="underlineOnHover" to="/">
+					Home
+				</NuxtLink>
+			</li>
+		</ul>
+	</nav>
+</template>
+
+<script lang="ts" setup>
+	const underlineOnHover = 'transition duration-300 ease-in-out hover:underline underline-offset-4';
+</script>
