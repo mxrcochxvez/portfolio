3c7d42089b0bee7c09b823d58de7ac9d7edfc517
Marco Chavez <marcode.chavez.jr@gmail.com>
2025-03-29 13:12:25 -0700

    feat(default): creates a default layout

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <main class="px-4 text-white">
+    <Navbar />
+    <slot />
+  </main>
+</template>
+
+<script lang="ts" setup>
+  import Navbar from '~/components/navbar.vue';
+</script>
+
+<style>
+  * {
+    background-color: rgb(46 16 101);
+  }
+</style>
