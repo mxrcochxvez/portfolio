cd3b0ac9ccdc8f2c8080c4824091c694802ec42a
Marco Chavez <marcode.chavez.jr@gmail.com>
2025-03-28 23:19:45 -0700

    removes nuxt/ui in favor of tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/fonts', '@nuxt/icon', '@nuxt/ui']
+  modules: ['@nuxt/fonts', '@nuxt/icon', '@nuxtjs/tailwindcss']
 })
