a718a7f4509f4ed011698799a77e83d2289d45c1
Marco Chavez <marcode.chavez.jr@gmail.com>
2025-03-29 20:20:25 -0700

    feat(index): uses a hero section to display the first section of the home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,20 @@
 <template>
 	<div>
-		<p>test</p>
+		<Hero
+			:heading-level="2"
+			heading-text="Strategy. Story. Then Sales."
+			:body-text="bodyText"
+			button-text="Begin your digital journey"
+			image-src="/img/storyboarding.jpg"
+			image-alt="An image of someone drafting up an idea"
+		/>
 	</div>
 </template>
 
 <script lang="ts" setup>
+	import Hero from '~/components/Hero.vue';
 
+	const bodyText = 'I help brands grow by crafting marketing strategies that connect, stories that resonate, and campaigns that convert. Whether you\'re building from scratch or scaling up, I bring clarity, creativity, and results to every stage of your journey.'
 </script>
 
 <style>
