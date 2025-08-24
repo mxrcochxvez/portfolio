51e96df9bebd8967f31356bb9918f3e985fe6748
Marco Chavez <marcode.chavez.jr@gmail.com>
2025-08-23 21:58:04 -0700

    feat(index): adds about me section

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,65 @@
 <template>
-	<div>
-		<Hero
-			:heading-level="2"
-			heading-text="Strategy. Story. Then Sales."
-			:body-text="bodyText"
-			button-text="Begin your digital journey"
-			image-src="/img/storyboarding.jpg"
-			image-alt="An image of someone drafting up an idea"
-		/>
-	</div>
+	<section :class="[$style.headingSection, 'bg-violet-950 text-white']">
+		<img :class="$style.bannerImage" :src="bannerImageUrl" alt="Strategy. Story. Then Sales." />
+
+		<div>
+			<p>With over 6 years of experience in the industry. In jobs related to ecommerce, SaaS, and internal tooling. I am an expert in accessibility, performance, and bringing your projects to the finish line.</p>
+			<a class="text-sm underline underline-offset-4 hidden md:inline-block" href="#about-me">Read More...</a>
+		</div>
+	</section>
+
+	<section id="about-me" :class="[$style.aboutMeSection, 'bg-white text-black']">
+		<Heading :level="2">About Me</Heading>
+
+		<div class="flex flex-col items-center gap-4 pt-4 md:flex-row md:gap-8">
+			<div class="flex flex-col items-center">
+				<img :class="$style.myImage" :src="myImageUrl" alt="" />
+				<Heading :level="3" class="mt-4 md:text-sm">Marco Chavez</Heading>
+			</div>
+
+			<div>
+				<p>Dependable and reliable software developer with a strong focus on accessibility, performance, and maintainability across the full stack. Experienced in leveraging modern technologies to deliver fast, efficient API integrations and building scalable component libraries for both mobile and web applications. Committed to mentorship, clear documentation, and collaborative practices that help teams grow while ensuring long-term project success.</p>
+			</div>
+		</div>
+	</section>
 </template>
 
 <script lang="ts" setup>
-	import Hero from '~/components/Hero.vue';
-
-	const bodyText = 'I help brands grow by crafting marketing strategies that connect, stories that resonate, and campaigns that convert. Whether you\'re building from scratch or scaling up, I bring clarity, creativity, and results to every stage of your journey.'
+	const bannerImageUrl = 'https://i.imgur.com/LcukdRk.png';
+	const myImageUrl = 'https://media.licdn.com/dms/image/v2/D5635AQGJhnw3xPTA0w/profile-framedphoto-shrink_400_400/B56ZjSt5n1IEAg-/0/1755881905006?e=1756594800&v=beta&t=kdr1zvPaTrKHmcH2J9XQ39_j9n7NpvDsZ8KYtkYpIBg';
 </script>
 
-<style>
+<style lang="scss" module>
+	.bannerImage {
+		max-width: 600px;
+	}
 
+	.headingSection {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		padding: 64px 24px;
+		gap: 24px;
+	}
+
+	.readMoreButton {
+		display: none;
+	}
+
+	.myImage {
+		max-width: 150px;
+		border-radius: 50%;
+	}
+
+	.aboutMeSection {
+		padding: 12px 24px;
+	}
+
+	@media (min-width: 600px) {
+		.headingSection {
+			flex-direction: row;
+			align-items: center;
+			font-size: 1.5rem;
+		}
+	}
 </style>
