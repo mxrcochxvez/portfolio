0632de8c15756154f9e66aecd1072d111b9adaf8
Marco Chavez <marcode.chavez.jr@gmail.com>
2025-10-19 00:55:53 -0700

    feat(server): includes a health-check

---
+++ components/api-status.vue
@@ -0,0 +1,22 @@
+<template>
+	<Suspense>
+		<template #default>
+			<div v-if="data?.status" class="fixed bottom-4 right-4 bg-green-200 text-green-800 py-2 px-4 rounded-full">
+				API status: {{ data.status }}
+			</div>
+			<div v-else-if="error" class="fixed bottom-4 right-4 bg-red-200 text-red-800 py-2 px-4 rounded-full">
+				API Down
+			</div>
+		</template>
+
+		<template #fallback>
+			<div class="fixed bottom-4 right-4 bg-yellow-200 text-yellow-800 py-2 px-4 rounded-full">
+				Checking API status...
+			</div>
+		</template>
+	</Suspense>
+</template>
+
+<script setup lang="ts">
+	const { data, error } = useFetch('/api/health-check');
+</script>
